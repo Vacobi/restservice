de36bc9a70aca2c981bdea02514090a6a72914bb
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-03-02 20:03:09 +0300

    feat: added test class for Service and implemented test for checking converting to XML only

---
+++ jsonconverter/src/test/java/axiomatika/converter/jsonconverter/service/RestJsonServiceTest.java
@@ -0,0 +1,57 @@
+package axiomatika.converter.jsonconverter.service;
+
+import axiomatika.converter.jsonconverter.config.TestContainersConfig;
+import axiomatika.converter.jsonconverter.repository.JsonRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static testutils.TestAsserts.assertTagsInXmlStringsAreEqualsWithoutOrder;
+
+@SpringBootTest
+@ContextConfiguration(initializers = TestContainersConfig.class)
+class RestJsonServiceTest {
+
+    private JsonRepository jsonRepository;
+
+    @Autowired
+    private RestJsonService restJsonService;
+
+    @Test
+    void convertCorrectJsonToXml() {
+        String jsonString = """
+                {
+                    "name": "Тест",
+                    "surname": "Тестов",
+                    "patronymic": "Тестович",
+                    "birthDate": "1990-01-01",
+                    "gender": "MAN",
+                    "document": {
+                        "series": "1333",
+                        "number": "112233",
+                        "type": "PASSPORT",
+                        "issueDate": "2020-01-01"
+                    },
+                }""";
+
+        String expXml = """
+                <person>
+                    <name>Тест</name>
+                    <surname>Тестов</surname>
+                    <patronymic>Тестович</patronymic>
+                    <birthDate>1990-01-01</birthDate>
+                    <gender>MAN</gender>
+                    <document>
+                        <series>1333</series>
+                        <number>112233</number>
+                        <type>PASSPORT</type>
+                        <issueDate>2020-01-01</issueDate>
+                    </document>
+                </person>""";
+
+        String actualXml = restJsonService.convertToXslt(jsonString);
+
+        assertTagsInXmlStringsAreEqualsWithoutOrder(expXml, actualXml);
+    }
+}
