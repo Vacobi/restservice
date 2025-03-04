68001dadc175d9c16580b81b38a40c36d4e56b24
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-03-04 22:28:19 +0300

    refactor: soap request building and response parsing moved to a separate class

---
+++ jsonconverter/src/main/java/axiomatika/converter/jsonconverter/builder/SoapMessagesBuilder.java
@@ -0,0 +1,71 @@
+package axiomatika.converter.jsonconverter.builder;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.json.JSONObject;
+import org.json.XML;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SoapMessagesBuilder {
+
+    private final String SOAP_REQUEST_URI;
+    private final String charset;
+    private final String soapRequestLocalName;
+    private final String elementName;
+    private final String soapResponseLocalName;
+    private final String responseElementName;
+
+    public SoapMessagesBuilder(@Qualifier("soapRequestUri") String SOAP_REQUEST_URI,
+                               @Qualifier("charset") String charset,
+                               @Qualifier("soapRequestLocalName") String soapRequestLocalName,
+                               @Qualifier("elementName") String elementName,
+                               @Qualifier("soapResponseLocalName") String soapResponseLocalName,
+                               @Qualifier("responseElementName") String responseElementName) {
+
+        this.SOAP_REQUEST_URI = SOAP_REQUEST_URI;
+        this.charset = charset;
+        this.soapRequestLocalName = soapRequestLocalName;
+        this.elementName = elementName;
+        this.soapResponseLocalName = soapResponseLocalName;
+        this.responseElementName = responseElementName;
+    }
+
+    public HttpPost buildXMLSoapRequest(String xml) {
+        HttpPost request = new HttpPost(SOAP_REQUEST_URI);
+        request.setHeader("Content-Type", String.format("text/xml; charset=%s", charset));
+        request.setHeader("SOAPAction", SOAP_REQUEST_URI);
+        request.setEntity(new StringEntity(buildSoapRequest(xml), charset));
+        return request;
+    }
+
+    private String buildSoapRequest(String CDATA) {
+        return String.format("""
+        <?xml version="1.0" encoding="%s"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:axi="%s">
+           <soapenv:Header/>
+           <soapenv:Body>
+              <axi:%s>
+                 <axi:%s><![CDATA[%s]]></axi:%s>
+              </axi:%s>
+           </soapenv:Body>
+        </soapenv:Envelope>
+        """, charset, SOAP_REQUEST_URI, soapRequestLocalName, elementName, CDATA, elementName, soapRequestLocalName);
+    }
+
+    public String extractBodyOfSoapResponse(String soapResponse) {
+        JSONObject bodyOfResponseByObj = XML.toJSONObject(soapResponse);
+        String bodyOfResponse;
+        try {
+            bodyOfResponse = bodyOfResponseByObj.getJSONObject("SOAP-ENV:Envelope")
+                    .getJSONObject("SOAP-ENV:Body")
+                    .getJSONObject(String.format("ns2:%s", soapResponseLocalName))
+                    .getString(String.format("ns2:%s", responseElementName));
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected structure of SOAP response", e);
+        }
+
+        return bodyOfResponse;
+    }
+}
--- jsonconverter/src/main/java/axiomatika/converter/jsonconverter/service/RestJsonService.java
+++ jsonconverter/src/main/java/axiomatika/converter/jsonconverter/service/RestJsonService.java
@@ -1,5 +1,6 @@
 package axiomatika.converter.jsonconverter.service;
 
+import axiomatika.converter.jsonconverter.builder.SoapMessagesBuilder;
 import axiomatika.converter.jsonconverter.dto.ConvertToXsltResult;
 import axiomatika.converter.jsonconverter.entity.Json;
 import axiomatika.converter.jsonconverter.entity.Xslt;
@@ -9,7 +10,6 @@ import org.apache.commons.text.StringEscapeUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -25,17 +25,18 @@ public class RestJsonService {
     private final JsonRepository jsonRepository;
     private final XsltRepository xsltRepository;
 
-    private final String SOAP_REQUEST_URI;
     private final String charset;
+
+    private final SoapMessagesBuilder soapMessagesBuilder;
 
     public RestJsonService(JsonRepository jsonRepository,
                            XsltRepository xsltRepository,
-                           @Qualifier("soapRequestUri") String SOAP_REQUEST_URI,
-                           @Qualifier("charset") String charset) {
+                           @Qualifier("charset") String charset,
+                           SoapMessagesBuilder soapMessagesBuilder) {
         this.jsonRepository = jsonRepository;
         this.xsltRepository = xsltRepository;
-        this.SOAP_REQUEST_URI = SOAP_REQUEST_URI;
         this.charset = charset;
+        this.soapMessagesBuilder = soapMessagesBuilder;
     }
 
     @Transactional
@@ -64,15 +65,12 @@ public class RestJsonService {
 
     private String toXslt(String xml) {
         try (CloseableHttpClient client = HttpClients.createDefault()) {
-            HttpPost request = new HttpPost(SOAP_REQUEST_URI);
-            request.setHeader("Content-Type", String.format("text/xml; charset=%s", charset));
-            request.setHeader("SOAPAction", SOAP_REQUEST_URI);
-            request.setEntity(new StringEntity(buildSoapRequest(xml), charset));
+            HttpPost request = soapMessagesBuilder.buildXMLSoapRequest(xml);
             try (CloseableHttpResponse response = client.execute(request)) {
                 HttpEntity entity = response.getEntity();
                 if (entity != null) {
                     String soapResponse = EntityUtils.toString(entity, charset);
-                    String codedBodyOfSoapResponse = extractBodyOfSoapResponse(soapResponse);
+                    String codedBodyOfSoapResponse = soapMessagesBuilder.extractBodyOfSoapResponse(soapResponse);
                     return decode(codedBodyOfSoapResponse);
                 }
                 return null;
@@ -82,37 +80,8 @@ public class RestJsonService {
         }
     }
 
-    private String buildSoapRequest(String xml) {
-        return String.format("""
-        <?xml version="1.0" encoding="UTF-8"?>
-        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:axi="%s">
-           <soapenv:Header/>
-           <soapenv:Body>
-              <axi:getXsltRequest>
-                 <axi:xmlData><![CDATA[%s]]></axi:xmlData>
-              </axi:getXsltRequest>
-           </soapenv:Body>
-        </soapenv:Envelope>
-        """, SOAP_REQUEST_URI, xml);
-    }
-
     private String decode(String encoded) {
         return StringEscapeUtils.unescapeXml(encoded);
-    }
-
-    private String extractBodyOfSoapResponse(String soapResponse) {
-        JSONObject bodyOfResponseByObj = XML.toJSONObject(soapResponse);
-        String bodyOfResponse;
-        try {
-            bodyOfResponse = bodyOfResponseByObj.getJSONObject("SOAP-ENV:Envelope")
-                    .getJSONObject("SOAP-ENV:Body")
-                    .getJSONObject("ns2:getXsltResponse")
-                    .getString("ns2:xsltResult");
-        } catch (Exception e) {
-            throw new RuntimeException("", e);
-        }
-
-        return bodyOfResponse;
     }
 }
 
