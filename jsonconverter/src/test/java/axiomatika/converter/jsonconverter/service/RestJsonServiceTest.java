16384059575f515864792002954ee9f4b5e04fe3
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-03-04 19:44:48 +0300

    test: added 'Autowired' annotation to 'JsonRepository'

--- jsonconverter/src/test/java/axiomatika/converter/jsonconverter/service/RestJsonServiceTest.java
+++ jsonconverter/src/test/java/axiomatika/converter/jsonconverter/service/RestJsonServiceTest.java
@@ -16,6 +16,7 @@ import static testutils.TestAsserts.assertTagsInXmlStringsAreEqualsWithoutOrder;
 @ContextConfiguration(initializers = TestContainersConfig.class)
 class RestJsonServiceTest {
 
+    @Autowired
     private JsonRepository jsonRepository;
 
     @Autowired
